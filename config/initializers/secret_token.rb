7940bd94c84b9785fa3f776e72af1015e829e0bc
Allie Jones <allie.jones@gmail.com>
2013-04-18 16:17:59 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Excorpse::Application.config.secret_token = '655b2574357061a86d67457e8be2a64930742faae754cc764ee4cef3ce6165e69a016792313624ddba84f7fdacc88f3b94e130315148eccf4cd62f4ebbd91acb'
